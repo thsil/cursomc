6386cdb194f06ec881ffdbe68724a2e321d32b10
thsil <albertthome@gmail.com>
2022-06-26 20:24:52 -0300

    Testando a primeira classe de donimio - Categoria

--- src/main/java/com/albsilv/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/albsilv/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.albsilv.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.albsilv.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@GetMapping
-	public String listar() {
-		return "Rest Ok!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
+		
 	}
 
 }
